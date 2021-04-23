12fb6811f4bdfd2ebb9069581a1fd093b5d857ea
Arina <rina.runet@gmail.com>
2021-04-23 16:07:16 +0300

    homework_1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 'use strict';
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = 'Unknown title', price = 0) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <div class="product-item-info">
+                    <h3>${title}</h3>
+                    <p>${price} ₽</p>
+                </div>
                 <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-}
+            </div>`;   
+};
 
-const renderProducts = (list) => {
-    const productListHTML = list.map((item) => renderProduct(item.title, item.price));
-    // console.log(productListHTML);
-    document.querySelector('.products').innerHTML = productListHTML;
-}
+const Catalog = {
+    products: [
+        { id: 1, title: 'Notebook', price: 20000 },
+        { id: 2, title: 'Mouse', price: 1500 },
+        { id: 3, title: 'Keyboard', price: 5000 },
+        { id: 4, title: 'Gamepad', price: 4500 },
+        // проверка на работоспособность значений по умолчанию
+        { id: 5 },
+    ],
+    renderProducts() {
+        const productListHTML = this.products.map((item) => renderProduct(item.title, item.price));
+        // запятая выводится из-за того, что в productListHTML содержится массив данных, разделенный запятой
+        // если просто напрямую вывести весь массив, то выведутся и запятые
+        // document.querySelector('.products').innerHTML = productListHTML;
+        // решить это можно, например, с помощью цикла, который вывод каждый элемент по отдельности
+        let productList = document.querySelector('.products');
+        for (let i = 0; i < productListHTML.length; i++) {
+            productList.insertAdjacentHTML('beforeend', productListHTML[i]);
+        };
+    }
+};
 
-renderProducts(products);
+Catalog.renderProducts();
+
+
